7289179660ab76936cddbf7ab1feabb1b1228398
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-15 02:11:08 +0100

    done task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+floor module
+"""
+
+
+def floor(n: float) -> int:
+    """
+    takes a float n as argument
+    and returns the floor of the float.
+    """
+    return int(n)
