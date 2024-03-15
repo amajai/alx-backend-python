8cec9808af1604db3008e7795bbe7dbc299a87c1
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-15 02:22:37 +0100

    done task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+sum_list module
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    takes a float n as argument and returns
+    the string representation of the float.
+    """
+    return sum(input_list)
