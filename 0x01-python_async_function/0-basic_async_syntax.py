caa2756d603bda972367c50b2cdb2d8b051156c4
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-18 17:18:15 +0100

    done task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -12,5 +12,6 @@ async def wait_random(max_delay=10):
     (max_delay, with a default value of 10) named wait_random
     that waits for a random delay between 0 and max_delay
     """
-    r = random.uniform(0, max_delay)
-    return await asyncio.sleep(r, r)
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
