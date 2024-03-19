1b99ba9b233b5869009c101ecf26aa3074b10fab
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-19 23:20:32 +0100

    done task 2 - 0-sync_generator

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Module documentation
+"""
+import asyncio
+import time
+
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Run time for four parallel comprehensions
+    """
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(), async_comprehension(),
+                         async_comprehension(), async_comprehension()
+                         )
+    end = time.perf_counter()
+    return end - start
